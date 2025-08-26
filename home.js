d1b62d3d7621bed02b49c4c6785d36a43b09c9e8
nurullah305 <snurullahstudentboy@gmail.com>
2025-08-26 18:26:56 +0900

    completed add money section

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234
+
 document.getElementById('add-money-btn').addEventListener("click", function (e) {
 e.preventDefault()
 console.log("add money btn clicked")
@@ -5,11 +7,25 @@ console.log("add money btn clicked")
  const accountNumber = document.getElementById("account-number").value
  const amount = parseInt(document.getElementById("add-amount").value)
 
- const pin =document.getElementById("add-pin").value
+ const pin = parseInt(document.getElementById("add-pin").value)
 
   const availableBalance = parseInt( document.getElementById("available-balance").innerText)
  
-  const totalNewAvailableBalance = amount=availableBalance
+if(accountNumber.length < 11){
+  alert("Please provide valid account number")
+  return;
+}
+ 
+if(pin !== validPin){
+  alert("Please provide valid pin number")
+  return;
+
+}
+
+  const totalNewAvailableBalance = amount+availableBalance
 
 document.getElementById("available-balance").innerText  = totalNewAvailableBalance
+
+
+
 })
